2882f3195dfb09c8812fb52c3844b064b65e525c
Gaston82 <cabezaartaza@hotmail.com>
2023-06-05 14:17:45 +0200

    Fix eslint config error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", ["xo", "prettier"]],
+  extends: ["xo", "prettier"],
   overrides: [
     {
       extends: ["xo-typescript", "prettier"],
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,16 @@
 import styles from "./HomePage.module.css";
 
-const HomePage = () => {
-  return (
-    <main className={styles.mainContent}>
-      <article className={styles.card}>
-        <i className={`fa-solid fa-tv ${styles.icon}`}></i>
-        <span>Popular Series</span>
-      </article>
-      <article className={styles.card}>
-        <i className={`fa-solid fa-clapperboard ${styles.icon}`}></i>
-        <span>Popular Movies</span>
-      </article>
-    </main>
-  );
-};
+const HomePage = () => (
+  <main className={styles.mainContent}>
+    <article className={styles.card}>
+      <i className={`fa-solid fa-tv ${styles.icon}`}></i>
+      <span>Popular Series</span>
+    </article>
+    <article className={styles.card}>
+      <i className={`fa-solid fa-clapperboard ${styles.icon}`}></i>
+      <span>Popular Movies</span>
+    </article>
+  </main>
+);
 
 export default HomePage;
